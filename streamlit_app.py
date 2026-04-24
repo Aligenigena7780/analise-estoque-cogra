778cb2e12318369464169e8de77228233a41e3f1
Aligenigena7780 <lucas2005r@gmail.com>
2026-04-24 15:16:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown("""
         font-weight: 900;
         margin-bottom: 0.2rem;
 
-        background: linear-gradient(90deg, #E20A13, #FF4A1A);
+        background: linear-gradient(90deg, #E20A13, #FFFFFF);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
         background-clip: text;
